1f60f4c54b3cee207d8d165d3eedbd2c49ce67a6
Toby Dylan Hocking <tdhock5@gmail.com>
2016-11-09 16:19:40 -0500

    test for error with NA scores fails

--- tests/testthat/test-errors.R
+++ tests/testthat/test-errors.R
@@ -17,3 +17,31 @@ test_that("data set with no negative labels is an error", {
     WeightedROC(y.hat, y, w)
   }, "only one label value")
 })
+
+test_that("NA labels is an error", {
+  y <- c(-1, NA, 1, 1)
+  w <- c(1, 1, 1, 1)
+  y.hat <- c(1, 2, 3, 4)
+  expect_error({
+    tp.fp <- WeightedROC(y.hat, y, w)
+  })
+})
+
+test_that("NA scores is an error", {
+  y <- c(-1, -1, 1, 1)
+  w <- c(1, 1, 1, 1)
+  y.hat <- c(1, NA, 3, 4)
+  expect_error({
+    tp.fp <- WeightedROC(y.hat, y, w)
+  }, "ROC curve undefined for NA scores")
+})
+
+test_that("NA weights is an error", {
+  y <- c(-1, -1, 1, 1)
+  w <- c(1, 1, NA, 1)
+  y.hat <- c(1, 2, 3, 4)
+  expect_error({
+    tp.fp <- WeightedROC(y.hat, y, w)
+  })
+})
+
