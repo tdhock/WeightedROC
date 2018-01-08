79c29af69401868c4d7b0b2e4d1066e6057c0528
Toby Dylan Hocking <tdhock5@gmail.com>
2018-01-08 12:46:43 -0500

    if require

--- R/ROC.R
+++ R/ROC.R
@@ -101,83 +101,84 @@ WeightedROC <- structure(function
     geom_line(aes(threshold, weighted.error, color=error.type),
               data=all.error)
 
-  library(ROCR)
-  library(pROC)
-  library(microbenchmark)
-  data(ROCR.simple, envir=environment())
-  ## Compare speed and plot ROC curves for the ROCR example data set.
-  microbenchmark(WeightedROC={
-    tp.fp <- with(ROCR.simple, WeightedROC(predictions, labels))
-  }, ROCR={
-    pred <- with(ROCR.simple, prediction(predictions, labels))
-    perf <- performance(pred, "tpr", "fpr")
-  }, pROC.1={
-    proc <- roc(labels ~ predictions, ROCR.simple, algorithm=1)
-  }, pROC.2={
-    proc <- roc(labels ~ predictions, ROCR.simple, algorithm=2)
-  }, pROC.3={
-    proc <- roc(labels ~ predictions, ROCR.simple, algorithm=3)
-  }, times=10)
-  perfDF <- function(p){
-    data.frame(FPR=p@x.values[[1]], TPR=p@y.values[[1]], package="ROCR")
+  if(require(microbenchmark) && require(ROCR) && require(pROC)){
+    
+    data(ROCR.simple, envir=environment())
+    ## Compare speed and plot ROC curves for the ROCR example data set.
+    microbenchmark(WeightedROC={
+      tp.fp <- with(ROCR.simple, WeightedROC(predictions, labels))
+    }, ROCR={
+      pred <- with(ROCR.simple, prediction(predictions, labels))
+      perf <- performance(pred, "tpr", "fpr")
+    }, pROC.1={
+      proc <- roc(labels ~ predictions, ROCR.simple, algorithm=1)
+    }, pROC.2={
+      proc <- roc(labels ~ predictions, ROCR.simple, algorithm=2)
+    }, pROC.3={
+      proc <- roc(labels ~ predictions, ROCR.simple, algorithm=3)
+    }, times=10)
+    perfDF <- function(p){
+      data.frame(FPR=p@x.values[[1]], TPR=p@y.values[[1]], package="ROCR")
+    }
+    procDF <- function(p){
+      data.frame(FPR=1-p$specificities, TPR=p$sensitivities, package="pROC")
+    }
+    roc.curves <- rbind(
+      data.frame(tp.fp[, c("FPR", "TPR")], package="WeightedROC"),
+      perfDF(perf),
+      procDF(proc))
+    ggplot()+
+      geom_path(aes(FPR, TPR, color=package, linetype=package),
+                data=roc.curves, size=1)+
+      coord_equal()
+    
+    ## Compare speed and plot ROC curves for the pROC example data set.
+    data(aSAH, envir=environment())
+    microbenchmark(WeightedROC={
+      tp.fp <- with(aSAH, WeightedROC(s100b, outcome))
+    }, ROCR={
+      pred <- with(aSAH, prediction(s100b, outcome))
+      perf <- performance(pred, "tpr", "fpr")
+    }, pROC.1={
+      proc <- roc(outcome ~ s100b, aSAH, algorithm=1)
+    }, pROC.2={
+      proc <- roc(outcome ~ s100b, aSAH, algorithm=2)
+    }, pROC.3={
+      proc <- roc(outcome ~ s100b, aSAH, algorithm=3)
+    }, times=10)
+    roc.curves <- rbind(
+      data.frame(tp.fp[, c("FPR", "TPR")], package="WeightedROC"),
+      perfDF(perf),
+      procDF(proc))
+    ggplot()+
+      geom_path(aes(FPR, TPR, color=package, linetype=package),
+                data=roc.curves, size=1)+
+      coord_equal()
+    
+    ## Compute a small ROC curve with 1 tie to show the diagonal.
+    y <- c(-1, -1, 1, 1)
+    y.hat <- c(1, 2, 3, 1)
+    microbenchmark(WeightedROC={
+      tp.fp <- WeightedROC(y.hat, y)
+    }, ROCR={
+      pred <- prediction(y.hat, y)
+      perf <- performance(pred, "tpr", "fpr")
+    }, pROC.1={
+      proc <- roc(y ~ y.hat, algorithm=1)
+    }, pROC.2={
+      proc <- roc(y ~ y.hat, algorithm=2)
+    }, pROC.3={
+      proc <- roc(y ~ y.hat, algorithm=3)
+    }, times=10)
+    roc.curves <- rbind(
+      data.frame(tp.fp[, c("FPR", "TPR")], package="WeightedROC"),
+      perfDF(perf),
+      procDF(proc))
+    ggplot()+
+      geom_path(aes(FPR, TPR, color=package, linetype=package),
+                data=roc.curves, size=1)+
+      coord_equal()
+
   }
-  procDF <- function(p){
-    data.frame(FPR=1-p$specificities, TPR=p$sensitivities, package="pROC")
-  }
-  roc.curves <- rbind(
-    data.frame(tp.fp[, c("FPR", "TPR")], package="WeightedROC"),
-    perfDF(perf),
-    procDF(proc))
-  ggplot()+
-    geom_path(aes(FPR, TPR, color=package, linetype=package),
-              data=roc.curves, size=1)+
-    coord_equal()
-  
-  ## Compare speed and plot ROC curves for the pROC example data set.
-  data(aSAH, envir=environment())
-  microbenchmark(WeightedROC={
-    tp.fp <- with(aSAH, WeightedROC(s100b, outcome))
-  }, ROCR={
-    pred <- with(aSAH, prediction(s100b, outcome))
-    perf <- performance(pred, "tpr", "fpr")
-  }, pROC.1={
-    proc <- roc(outcome ~ s100b, aSAH, algorithm=1)
-  }, pROC.2={
-    proc <- roc(outcome ~ s100b, aSAH, algorithm=2)
-  }, pROC.3={
-    proc <- roc(outcome ~ s100b, aSAH, algorithm=3)
-  }, times=10)
-  roc.curves <- rbind(
-    data.frame(tp.fp[, c("FPR", "TPR")], package="WeightedROC"),
-    perfDF(perf),
-    procDF(proc))
-  ggplot()+
-    geom_path(aes(FPR, TPR, color=package, linetype=package),
-              data=roc.curves, size=1)+
-    coord_equal()
-  
-  ## Compute a small ROC curve with 1 tie to show the diagonal.
-  y <- c(-1, -1, 1, 1)
-  y.hat <- c(1, 2, 3, 1)
-  microbenchmark(WeightedROC={
-    tp.fp <- WeightedROC(y.hat, y)
-  }, ROCR={
-    pred <- prediction(y.hat, y)
-    perf <- performance(pred, "tpr", "fpr")
-  }, pROC.1={
-    proc <- roc(y ~ y.hat, algorithm=1)
-  }, pROC.2={
-    proc <- roc(y ~ y.hat, algorithm=2)
-  }, pROC.3={
-    proc <- roc(y ~ y.hat, algorithm=3)
-  }, times=10)
-  roc.curves <- rbind(
-    data.frame(tp.fp[, c("FPR", "TPR")], package="WeightedROC"),
-    perfDF(perf),
-    procDF(proc))
-  ggplot()+
-    geom_path(aes(FPR, TPR, color=package, linetype=package),
-              data=roc.curves, size=1)+
-    coord_equal()
   
 })
