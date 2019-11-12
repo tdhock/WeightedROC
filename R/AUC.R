10cdeba8a5b4d2bbbdb540199b045d02db4e09be
Toby Dylan Hocking <Toby.Hocking@nau.edu>
2019-11-12 13:29:16 -0700

    remove glmnet::auc ex

--- R/AUC.R
+++ R/AUC.R
@@ -29,48 +29,8 @@ WeightedAUC <- structure(function
   tp.fp <- WeightedROC(y.hat, y, w)
   (wauc <- WeightedAUC(tp.fp))
 
-  if(require(glmnet)){
-
-    ## glmnet 1.9-5 (2013-8-1) also has a function called auc, with a
-    ## third argument "w" that seems to be for weights. In a data set
-    ## with no tied scores, glmnet::auc is the same as WeightedAUC.
-    y <- c(0, 0, 1, 0, 1)
-    y.hat <- c(1, 2, 3, 4, 5)
-    w <- c(1, 1, 1, 4, 5)
-    rbind(
-      `glmnet::auc`=c(
-        missing.weights=glmnet::auc(y, y.hat),
-        unequal.weights=glmnet::auc(y, y.hat, w),
-        equal.weights=glmnet::auc(y, y.hat, rep(1, length(y)))),
-      `WeightedROC::WeightedAUC`=c(
-        WeightedAUC(WeightedROC(y.hat, y)),
-        WeightedAUC(WeightedROC(y.hat, y, w)),
-        WeightedAUC(WeightedROC(y.hat, y, rep(1, length(y))))))
-    
-    ## In a data set with tied scores, using glmnet::auc(y,prob) with
-    ## a missing third argument gives the same result as WeightedAUC,
-    ## but glmnet::auc(y,prob,w) does not.
-    set.seed(1)
-    y <- c(0, 0, 1, 1, 1)
-    w <- c(1, 1, 1, 4, 5)
-    y.hat <- c(1, 2, 3, 1, 1)
-    rbind(
-      `glmnet::auc`=c(
-        missing.weights=glmnet::auc(y, y.hat),
-        unequal.weights=glmnet::auc(y, y.hat, w),
-        equal.weights=glmnet::auc(y, y.hat, rep(1, length(y)))),
-      `WeightedROC::WeightedAUC`=c(
-        WeightedAUC(WeightedROC(y.hat, y)),
-        wauc,
-        WeightedAUC(WeightedROC(y.hat, y, rep(1, length(y))))))
-    
-  }
-
   ## For the un-weighted ROCR example data set, verify that our AUC is
-  ## the same as that of ROCR/pROC/glmnet. Note that glmnet::auc is
-  ## faster than WeightedROC::WeightedAUC because glmnet::auc it does
-  ## not include the overhead of computing a data.frame(TPR, FPR) that
-  ## could be plotted.
+  ## the same as that of ROCR/pROC.
   if(require(microbenchmark) && require(ROCR) && require(pROC)){
     data(ROCR.simple, envir=environment())
     microbenchmark(WeightedROC={
@@ -81,16 +41,12 @@ WeightedAUC <- structure(function
       rocr <- performance(pred, "auc")@y.values[[1]]
     }, pROC={
       proc <- pROC::auc(labels ~ predictions, ROCR.simple, algorithm=2)
-    }, glmnet={
-      gnet <- with(ROCR.simple, {
-        glmnet::auc(labels, predictions, rep(1, length(labels)))
-      })
     }, times=10)
-    rbind(WeightedROC=wroc, ROCR=rocr, pROC=proc, glmnet=gnet) #same
+    rbind(WeightedROC=wroc, ROCR=rocr, pROC=proc) #same
   }
 
   ## For the un-weighted pROC example data set, verify that our AUC is
-  ## the same as that of ROCR/pROC/glmnet. 
+  ## the same as that of ROCR/pROC.
   data(aSAH, envir=environment())
   table(aSAH$s100b)
   if(require(microbenchmark)){
@@ -102,12 +58,8 @@ WeightedAUC <- structure(function
       rocr <- performance(pred, "auc")@y.values[[1]]
     }, pROC={
       proc <- pROC::auc(outcome ~ s100b, aSAH, algorithm=2)
-    }, glmnet={
-      gnet <- with(aSAH, {
-        glmnet::auc(ifelse(outcome=="Good", 0, 1), s100b, rep(1, length(s100b)))
-      })
     }, times=10)
-    rbind(WeightedROC=wroc, ROCR=rocr, pROC=proc, glmnet=gnet)
+    rbind(WeightedROC=wroc, ROCR=rocr, pROC=proc)
   }
-  
+
 })
--- build.sh
+++ build.sh
@@ -1,9 +1,28 @@
 #!/bin/bash
+set -o errexit
+PREGEX="^Package: "
+PKG=$(grep $PREGEX DESCRIPTION|sed "s/$PREGEX//")
+echo Package from DESCRIPTION: $PKG
+R -e "if(require(inlinedocs))package.skeleton.dx('.')"
 cd ..
-rm -rf WeightedROC-release
-cp -r WeightedROC WeightedROC-release
-##grep -v WeightedROCData WeightedROC/DESCRIPTION | grep -v Remotes > WeightedROC-release/DESCRIPTION
-##rm WeightedROC-release/tests/testthat/test-WeightedROCData.R
-PKG_TGZ=$(R CMD build WeightedROC-release|grep building|sed 's/.*‘//'|sed 's/’.*//')
-R CMD INSTALL $PKG_TGZ
-R CMD check --as-cran $PKG_TGZ
+
+RELEASE=$PKG-release
+echo Copying $PKG to $RELEASE
+rm -rf $RELEASE
+cp -r $PKG $RELEASE
+
+echo Editing $RELEASE for CRAN submission
+grep -v Remotes $PKG/DESCRIPTION > $RELEASE/DESCRIPTION
+# rm $RELEASE/tests/testthat/*
+# cp $PKG/tests/testthat/test-CRAN*.R $RELEASE/tests/testthat
+
+echo Building $RELEASE
+RCMD="R --vanilla CMD"
+$RCMD build $RELEASE | tee build.out
+PKG_TGZ=$(grep building build.out|sed "s/.*\($PKG.*.tar.gz\).*/\1/")
+
+echo Installing $PKG_TGZ
+$RCMD INSTALL $PKG_TGZ
+
+echo Checking $PKG_TGZ
+$RCMD check --as-cran $PKG_TGZ
